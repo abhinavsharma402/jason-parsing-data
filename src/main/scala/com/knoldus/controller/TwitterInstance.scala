80adeac4fdd6d54a6103a77a11b1f1a979f7c5ff
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-03-05 17:02:35 +0530

    update

--- src/main/scala/com/knoldus/controller/TwitterInstance.scala
+++ src/main/scala/com/knoldus/controller/TwitterInstance.scala
@@ -8,11 +8,7 @@ import twitter4j.{Twitter, TwitterFactory}
 object TwitterInstance extends TwitterApi {
   override def getTwitterInstance: Twitter = {
 
-    //    val consumerKey = "e6uS4phTxImI68qlA6h4V3zwR"
-    //    val consumerSecret = "M8b4Q3sudgU9mNZgJx1onUlqQYi5h5YCK1GVacjAc8yHDAohFc"
-    //    val token = "160922224-AKOoOasbqi3huqT7uyq4Og0Oqlucn8rKeD9IcUvU"
-    //    val tokenSecret = "7HgIJUmjOX2AZThvVp7RPWsZwOrW1ffpvkEpjeBSQynnH"
-
+    
     val config = ConfigFactory.load()
     val twitter: Twitter = new TwitterFactory().getInstance() // Authorising with your Twitter Application credentials
     twitter.setOAuthConsumer(config.getString("consumer.key"),
